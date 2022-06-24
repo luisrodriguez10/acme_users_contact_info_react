81b237feed94205b47052ad99d404c80f8f13d23
Luis Rodriguez <luisu_10@hotmail.com>
2022-06-23 21:43:18 -0500

    removed comments

--- src/Components/UserInformation.js
+++ src/Components/UserInformation.js
@@ -30,48 +30,4 @@ const UserInformation = ({userSelected, users}) => {
 
 }
 
-// class UserInformation extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       user: {},
-//     };
-//   }
-
-//   async componentDidMount() {
-//     const response = await axios.get(`/api/users/${this.props.userSelected}`);
-//     const user = response.data;
-//     this.setState({ user });
-//   }
-
-  
-
-//   render() {
-//     const { user } = this.state;
-    
-//     return (
-//       <table>
-//         <tbody>
-//           <tr>
-//             <th>Name</th>
-//             <th>Phone</th>
-//             <th>Email</th>
-//             <th>Adjective</th>
-//             <th>Vehicle</th>
-//             <th>Bio</th>
-//           </tr>
-//           <tr>
-//             <td>{user.name}</td>
-//             <td>{user.phone}</td>
-//             <td>{user.email}</td>
-//             <td>{user.adj}</td>
-//             <td>{user.vehicle}</td>
-//             <td>{user.bio}</td>
-//           </tr>
-//         </tbody>
-//       </table>
-//     );
-//   }
-// }
-
 export default UserInformation;
